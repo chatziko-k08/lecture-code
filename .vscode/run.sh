0d199f36ee6ca8abae2d011aa5a73781c9a5b986
Kostas Chatzikokolakis <kostas@chatzi.org>
2025-05-21 12:49:04 +0300

    vscode: properly set cwd, cleaner segfault messages

--- .vscode/run.sh
+++ .vscode/run.sh
@@ -8,9 +8,23 @@ then
 	echo " *  Executing: $CODE_RUN"
 	echo
 
+	# If the child process launched by eval $CODE_RUN below	segfaults, bash by
+	# default prints the current script's (run.sh) full path and line number,
+	# before the "Segmentation fault" message. We can make the error message cleaner
+	# by using the "trap" below.
+	#
+	# NOTE1: the trap handler is not really executed since the segfault happens
+	# in a child process. However the presence of the trap somehow causes bash
+	# not to print the script name, so the message becomes cleaner. We still
+	# echo a relevant message, just in case the trap does run on some system.
+	#
+	# NOTE2: SIGABRT does the same for asserts
+	#
+	trap 'echo "Segmentation fault"' SIGSEGV SIGABRT
+
 	# we use "eval" to allow for input/output redirections
-	cd "$CODE_DIR"
 	eval $CODE_RUN
+
 else
 	echo " *  Executing: $CODE_DEBUG"
 	echo
